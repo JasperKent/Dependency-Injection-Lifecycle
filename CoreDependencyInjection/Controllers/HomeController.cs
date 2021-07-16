94b86a4c927808746274fc6bfbfd52ecadf15a2e
Jasper Kent <jasper_kent@hotmail.com>
2021-07-16 08:40:44 +0100

    Add project files.

---
+++ CoreDependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using CoreDependencyInjection.Models;
+using CoreDependencyInjection.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoreDependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBookReviewRepository _repository;
+        private readonly IReviewAggregator _aggregator;
+ 
+        public HomeController(IBookReviewRepository repository, IReviewAggregator aggregator)
+        {
+            _repository = repository;
+            _aggregator = aggregator;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.All);
+        }
+
+        public IActionResult Summary()
+        {
+            return View("Index", _aggregator.Summary);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoreDependencyInjection/Services/BookReviewRepository.cs
@@ -0,0 +1,37 @@
+﻿using CoreDependencyInjection.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreDependencyInjection.Services
+{
+    public class BookReviewRepository : IBookReviewRepository
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        private BookReview[] _fakeData = new[]
+        {
+            new BookReview { Title = "Dr No", Rating = 1 },
+            new BookReview { Title = "Dr No", Rating = 2 },
+            new BookReview { Title = "Dr No", Rating = 4 },
+            new BookReview { Title = "Goldfinger", Rating = 5 },
+            new BookReview { Title = "Goldfinger", Rating = 4 },
+            new BookReview { Title = "Goldfinger", Rating = 3 },
+            new BookReview { Title = "Emma", Rating = 5 },
+            new BookReview { Title = "Emma", Rating = 5 }
+        };
+
+        // private readonly BookReviewDbContext _dbContext;
+
+        public BookReviewRepository(BookReviewDbContext dbContext)
+        {
+            //_dbContext = dbContext;
+        }
+
+        //public IEnumerable<BookReview> All => _dbContext.BookReviews;
+        //public IEnumerable<BookReview> ByTitle(string title) => _dbContext.BookReviews.Where(r => r.Title == title);
+        
+        public IEnumerable<BookReview> All => _fakeData;
+        public IEnumerable<BookReview> ByTitle(string title) => _fakeData.Where(r => r.Title == title);
+    }
+}
